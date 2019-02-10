45c2c5c0bec4019e260a79d368a855eb46a22c74
VovaNguyen11 <vovanguyen11@gmail.com>
2019-02-11 00:21:47 +0200

    done projects section

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,7 @@
             arrows: false,
             dots: true,
             slidesToScroll: 1,
+            autoplay:true,
             autoplaySpeed: 4000,
             infinite: false
         })
