8ac90daed6dff88440a607c91e1daa5807dec067
VovaNguyen11 <vovanguyen11@gmail.com>
2019-02-12 18:25:56 +0200

    chaged css file structure and done gallery section

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,12 +2,52 @@
     $(document).ready(() => {
         $('.js-news-slider').slick({
             slidesToShow: 3,
-            arrows: false,
-            dots: true,
             slidesToScroll: 1,
-            autoplay:true,
-            autoplaySpeed: 4000,
-            infinite: false
-        })
-    })
+            dots:true,
+            autoplay: true,
+            autoplaySpeed: 2000,
+            infinite: false,
+            swipeToSlide: false,
+            draggable: false,
+            responsive: [
+                {
+                  breakpoint: 1300,
+                  settings: {
+                    arrows: false,
+                  }
+                },
+                {
+                  breakpoint: 600,
+                  settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                  }
+                },
+                {
+                  breakpoint: 480,
+                  settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                  }
+                }
+              ]
+        });
+    });
+
+    $('[data-fancybox="gallery"]').fancybox({
+        animationEffect: 'zoom-in-out',
+        transitionEffect: 'slide'
+    });
+
+
+    // window.activeGMaps = [];
+    // const $map = $('.map__container');
+    // const map = new google.maps.Map(
+    //     $map[0], {
+    //         center: {
+    //             lat: 41.00270267,
+    //             lng: -74.24972534
+    //         },
+    //         zoom: 10
+    //     });
 })(jQuery)
