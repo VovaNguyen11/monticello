16e9f56aa62244b7f50e3afe23891cb3d680edf5
VovaNguyen11 <vovanguyen11@gmail.com>
2019-02-15 19:40:17 +0200

    done contact section

---
+++ assets/js/plugins.js
@@ -0,0 +1,43 @@
+(($) => {
+    $(document).ready(() => {
+        $('.js-news-slider').slick({
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            dots: true,
+            autoplay: true,
+            autoplaySpeed: 4000,
+            infinite: false,
+            responsive: [{
+                    breakpoint: 1400,
+                    settings: {
+                        slidesToShow: 3,
+                        arrows: false,
+                        draggable: false,
+                    }
+                },
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 2,
+                        arrows: false,
+                        draggable: true,
+                    }
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 1,
+                        arrows: false,
+                        draggable: true
+                    }
+                }
+            ]
+        });
+    });
+
+    $('[data-fancybox="gallery"]').fancybox({
+        animationEffect: 'zoom-in-out',
+        transitionEffect: 'slide'
+    });
+
+})(jQuery)
